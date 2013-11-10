5d99260f10c6574d64796bec138e513c5df4f8e3
siddharthan64 <siddharthan64@gmail.com>
2013-11-10 13:24:51 -0800

    Initial Commit

---
+++ semantics3-objc/semantics3_objc.h
@@ -0,0 +1,13 @@
+//
+//  semantics3_objc.h
+//  semantics3-objc
+//
+//  Created by Siddharthan Asokan on 10/11/13.
+//  Copyright (c) 2013 Semantics3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface semantics3_objc : NSObject
+
+@end
---
+++ semantics3-objc/semantics3_objc.m
@@ -0,0 +1,13 @@
+//
+//  semantics3_objc.m
+//  semantics3-objc
+//
+//  Created by Siddharthan Asokan on 10/11/13.
+//  Copyright (c) 2013 Semantics3. All rights reserved.
+//
+
+#import "semantics3_objc.h"
+
+@implementation semantics3_objc
+
+@end
