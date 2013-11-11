76ac208bf6b0d1e1bf31347d6603904655621d85
siddharthan64 <siddharthan64@gmail.com>
2013-11-11 00:28:54 -0800

    added sample project

---
+++ Semantics3-iOS-Demo/Semantics3-iOS-Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Semantics3-iOS-Demo
+//
+//  Created by Siddharthan Asokan on 10/11/13.
+//  Copyright (c) 2013 Semantics3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
